7163d5d55bf87bf30c57ba2e596538c1ce56cbbd
Surya <s8296659@gmail.com>
2026-04-18 20:20:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-"use client";
 
 export default function Home() {
   return (
-      <main style={{
-            height: "200vh",
-                  background: "#050505",
-                        color: "white",
-                              display: "flex",
-                                    alignItems: "center",
-                                          justifyContent: "center"
-                                              }}>
-                                                    <h1>🔥 Playful is Live 🚀</h1>
-                                                        </main>
-                                                          );
-                                                        }
+    <main
+      style={{
+        minHeight: "200vh",
+        background: "#050505",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Playful is Live 🚀</h1>
+    </main>
+  );
+}
