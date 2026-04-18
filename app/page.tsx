a35c34aee90b191086ee362721f405fff53cb315
Surya <s8296659@gmail.com>
2026-04-18 14:48:14 +0000

    fix again

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Home() {
+  return (
+      <main style={{
+            height: "200vh",
+                  background: "#050505",
+                        color: "white",
+                              display: "flex",
+                                    alignItems: "center",
+                                          justifyContent: "center"
+                                              }}>
+                                                    <h1>🔥 Playful is Live 🚀</h1>
+                                                        </main>
+                                                          );
+                                                        }
